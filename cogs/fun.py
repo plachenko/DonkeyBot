c09e8a2b9a344ca295b79676de18cc514b540bfa
OK J <owenkj@hotmail.ca>
2020-06-05 18:58:39 -0400

    experiment fixing

--- cogs/fun.py
+++ cogs/fun.py
@@ -21,6 +21,13 @@ class FunCog(commands.Cog):
 
         with open("data/combo.txt", "r") as f:
             self.combo = f.read()
+    
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if (before.channel.id == 717886687715262554):
+            beforeCount = before.content.split(" ", 1)[0]
+            if (not after.content.startswith(beforeCount + " ")):
+                await before.author.edit(nick="(" + beforeCount + ") im an idiot")
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -69,42 +76,64 @@ class FunCog(commands.Cog):
         
         if (message.channel.id == 718251019661869156):
 
-            count = int(self.combo)
-            nextCountStr = str(count+1)
+            if (member.guild_permissions.manage_messages == False):
+                count = int(self.combo)
+                nextCountStr = str(count+1)
 
-            if (message.content.startswith(nextCountStr + " ") or (message.content == nextCountStr)):
-                self.combo = count + 1
-                with open("data/combo.txt", "w") as f:
-                    f.write(str(self.combo))
-            elif (not message.author.bot):
-                best = message.channel.topic.split("Best: ", 1)[1]
+                if (message.content.startswith(nextCountStr + " ") or (message.content == nextCountStr)):
+                    self.combo = count + 1
+                    with open("data/combo.txt", "w") as f:
+                        f.write(str(self.combo))
+                    
+                    #Update Roles
+                    memberRoles = message.author.roles
+                    goodRole = message.guild.get_role(718590952947580950)
+                    if (goodRole not in memberRoles):
+                        await message.author.add_roles(goodRole)
+                    
+                elif (not message.author.bot):
+                    best = message.channel.topic.split("Best: ", 1)[1]
 
-                countdownMessage = "<@" + str(message.author.id) + "> broke <#718251019661869156> <:luigisad:406759665058185226>"
-                if (count > int(best)):
-                    countdownMessage += " **(NEW BEST: " + str(count) + ")**"
-                    await message.channel.edit(topic="Best: " + str(count))
+                    countdownMessage = "<@" + str(message.author.id) + "> broke <#718251019661869156> <:luigisad:406759665058185226>"
+                    if (count > int(best)):
+                        countdownMessage += " **(NEW BEST: " + str(count) + ")**"
+                        await message.channel.edit(topic="Best: " + str(count))
 
-                #Countdown
-                timer = 10
-                countdown = await message.channel.send(countdownMessage + "\nResetting in " + str(timer) + "...")
+                    #Countdown
+                    timer = 10
+                    countdown = await message.channel.send(countdownMessage + "\nResetting in " + str(timer) + "...")
 
-                for i in range(timer):
-                    time.sleep(1)
-                    await countdown.edit(content=countdownMessage + "\nResetting in " + str(timer) + "...")
-                    timer -= 1
-                
-                notifChannel = message.guild.get_channel(284028535155326976) #General
-                await notifChannel.send(countdownMessage)
+                    for i in range(timer):
+                        time.sleep(1)
+                        await countdown.edit(content=countdownMessage + "\nResetting in " + str(timer) + "...")
+                        timer -= 1
+                    
+                    notifChannel = message.guild.get_channel(517129265611800588) #Lore
+                    await notifChannel.send(countdownMessage + "\n> " + message.content)
 
-                #Delete all messages in channel
-                messagesDeleted = await message.channel.purge(limit=100)
-                while (len(messagesDeleted) != 0):
+                    #Update Roles
+                    memberRoles = message.author.roles
+                    goodRole = message.guild.get_role(718590952947580950)
+                    badRole = message.guild.get_role(718590813726179398)
+
+                    if (goodRole in memberRoles):
+                        await message.author.remove_roles(goodRole)
+                    
+                    await message.author.add_roles(badRole)
+
+                    #Delete all messages in channel
                     messagesDeleted = await message.channel.purge(limit=100)
+                    while (len(messagesDeleted) != 0):
+                        messagesDeleted = await message.channel.purge(limit=100)
 
-                #Reset
-                with open("data/combo.txt", "w") as f:
-                    f.write("0")
-                self.combo = 0
+                    #Reset
+                    with open("data/combo.txt", "w") as f:
+                        f.write("0")
+                    self.combo = 0
+            else:
+                await message.delete()
+                await message.author.send("You cannot participate in experiment because you bypass slowmode.")
+
 
 
 
