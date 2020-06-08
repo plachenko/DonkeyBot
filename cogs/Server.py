ce67601375c58faa7c40bb6cf0e4166fe57236b5
OK J <owenkj@hotmail.ca>
2020-06-07 22:13:46 -0400

    Create Server.py

---
+++ cogs/Server.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Server():
+    def __init__(self):
+
+        #Server
+        self.server = 284028259027648513
+
+        #Channels
+        self.generalChannel = 284028535155326976
+        self.experimentChannel = 718251019661869156
+        self.labChannel = 718600409652133948
+
+        """         
+        testChannel = 717886687715262554
+        - Replace any channels with this for testing purposes 
+        """
+
+        #Roles
+        self.goodRole = 718590952947580950
+        self.badRole = 718590813726179398
+        self.coolGuyRole = 717900752583917598
+
+        #Important People
+        self.robID = 151486808247500801
+
