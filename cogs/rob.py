a5860286ba1fd849c4cbd2d1378bf8d8658c4bb3
OK J <owenkj@hotmail.ca>
2020-06-07 22:13:41 -0400

    inherits Server

--- cogs/rob.py
+++ cogs/rob.py
@@ -1,21 +1,24 @@
 import discord
 from discord.ext import commands
+from .Server import Server
 
 from random import seed
 from random import choice
 
-def isRob(ctx):
-    if (ctx.message.author.id == 151486808247500801):
+def isRob(ctx, robID):
+    if (ctx.message.author.id == robID):
         return True
     return False
 
-class RobCog(commands.Cog):
+class RobCog(commands.Cog, Server):
     def __init__(self, client):
         self.client = client
+        Server.__init__(self)
 
     @commands.command()
     async def raffle(self, ctx):
-        if isRob(ctx):
+        
+        if isRob(ctx, self.robID):
             await ctx.message.delete() #Delete command usage
 
             #Get active users
@@ -29,6 +32,8 @@ class RobCog(commands.Cog):
                 selection = choice(activeUsers)
                 if ctx.guild.get_member(int(selection)) != None:
                     found = True
+
+            await ctx.guild.get_member(int(selection)).add_roles(ctx.guild.get_role(self.coolGuyRole))
             await ctx.send("<@" + selection + "> won the cool guy raffle")
 
 def setup(client):
