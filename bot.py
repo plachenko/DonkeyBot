22f031c9ebfcf9736da0914a480afd7606b7c1f7
OK J <owenkj@hotmail.ca>
2020-06-11 14:32:45 -0400

    load cogs after bot has logged

--- bot.py
+++ bot.py
@@ -10,14 +10,14 @@ client = commands.Bot(command_prefix=commands.when_mentioned, help_command=None)
 
 initial_extensions = ['cogs.basic','cogs.fun','cogs.experiment','cogs.rob','cogs.lab'] #Add cog filenames here
 
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        client.load_extension(extension)
-
 @client.event
 async def on_ready():
     print("Logged in")
     game = discord.Game("ALL HAIL ROB") #Never ever change this because it is the truth
     await client.change_presence(activity=game)
+
+    if __name__ == '__main__':
+        for extension in initial_extensions:
+            client.load_extension(extension)
 
 client.run(os.getenv("BOT_TOKEN"), reconnect=True)
